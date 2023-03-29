fa220ebfd6a31083796cd52e845b51307afd2fbe
CY <17103513@qq.com>
2023-03-29 11:40:33 +0800

    update version file
    Signed-off-by: CY <17103513@qq.com>

---
+++ pGerrit/__version__.py
@@ -0,0 +1,9 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = 'Gerrit'
+__description__ = 'Python lib for gerrit RestAPI.'
+__version__ = '0.1.0'
+__author__ = 'CY'
+__author_email__ = '17103513@qq.com'
