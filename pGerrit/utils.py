630a515e1f15ddddd766d4df73a71e8911308f72
Cheng Yang <17103513@qq.com>
2023-03-30 01:06:22 +0800

    add a cookie file helper function
    add a helper function to parse git cookiefile
    into dict
    
    Signed-off-by: Cheng Yang <17103513@qq.com>

--- pGerrit/utils.py
+++ pGerrit/utils.py
@@ -1,4 +1,5 @@
 from urllib.parse import quote
+import re
 
 def urljoin(*args):
     return "/".join(map(lambda x: str(x).rstrip('/').lstrip('/'), args)) + ("/" if args[len(args) - 1].endswith("/") else "")
@@ -6,6 +7,18 @@ def urljoin(*args):
 def urlformat(formattedString, *args):
     args = [quote(str(arg), safe="") for arg in args]
     return formattedString.format(*args)
+
+def parseCookieFile(cookiefile):
+    """Parse a cookies.txt file and return a dictionary of key value pairs
+    compatible with requests."""
+
+    cookies = {}
+    with open (cookiefile, 'r') as fp:
+        for line in fp:
+            if not re.match(r'^\#', line):
+                lineFields = line.strip().split('\t')
+                cookies[lineFields[5]] = lineFields[6]
+    return cookies
         
 
 # def simpleDictToObject(dict, Class):
